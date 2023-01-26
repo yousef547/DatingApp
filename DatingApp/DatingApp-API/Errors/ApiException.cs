2baf0bc944bb20cc4ff6488dd07f4385d3631df7
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-01-26 18:41:01 +0200

    complate Error handling in server

---
+++ DatingApp/DatingApp-API/Errors/ApiException.cs
@@ -0,0 +1,17 @@
+﻿namespace DatingApp_API.Errors
+{
+    public class ApiException
+    {
+        public ApiException(int statusCode,string message=null, string details=null)
+        {
+            StatusCode = statusCode;
+            Message = message;  
+            Details = details;
+        }
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        public string Details { get; set; }
+
+    }
+}
--- DatingApp/DatingApp-API/Startup.cs
+++ DatingApp/DatingApp-API/Startup.cs
@@ -1,6 +1,7 @@
 using DatingApp_API.Data;
 using DatingApp_API.Extensions;
 using DatingApp_API.Interface;
+using DatingApp_API.Middleware;
 using DatingApp_API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -76,12 +77,10 @@ namespace DatingApp_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DatingApp_API v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DatingApp_API v1"));
 
             app.UseHttpsRedirection();
 
