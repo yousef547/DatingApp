b2f878006314bc6b470639cd3116c104aec880fe
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-03-31 11:49:58 +0200

    Done `Building the User Interface`

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,9 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_inerceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberListComponent } from './member/member-list/member-list.component';
+import { MemberCardComponent } from './member/member-card/member-card.component';
+import { MemberDetailsComponent } from './member/member-details/member-details.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,10 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberListComponent,
+    MemberCardComponent,
+    MemberDetailsComponent
   ],
   imports: [
     BrowserModule,
